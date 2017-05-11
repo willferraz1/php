0941a8a818e809f2b9d3b2195cb293a87ad0c710
WILL Ferraz <will-ferraz@hotmail.com>
2017-05-10 21:59:35 -0300

    descrevendo o ultimo comando do git para enviar as mudanças realizadas no arquivo para o servidor do github

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
 
 	echo "Olá Udemy e olá Hcode!";
 
+	echo "Comandos do git que utilizado para controle de versao e para armazenar as mudanças realizadas em um arquivo em no servidor de armazenamento do github ou bitbucked";
+
 	echo "O que digitar quando terminar de editar um arquivo?";
 
 	echo "Apos terminar de editar o arquivo digitar o seguinte comando git add --all";
 
 	echo "Em seguida digitar o comando git commit -m e colocar aspas duplas para a mensagem";
+
+	echo "O ultimo comando é o git push origin master, que serve para enviar as alteraçoes feitas no arquivo para o servidor do github";
 
 ?>	
